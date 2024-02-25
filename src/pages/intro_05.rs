325963a28daf78c60844fac260a8575a6dba697a
Hudi304 <hudisteanum@yahoo.com>
2024-02-25 16:58:29 +0200

    Implement intro_05 borrow checker rules

---
+++ src/pages/intro_05.rs
@@ -0,0 +1,15 @@
+use leptos::*;
+
+use crate::components::page_layout::PageLayout;
+
+#[component]
+pub fn TheBorrowChecker2() -> impl IntoView {
+    view! {
+        <PageLayout>
+            <div class="text-orange-500 text-7xl font-bold">"The Borrow Checker"</div>
+            <div>"Two rules"</div>
+            <div>"1) Data has one owner"</div>
+            <div>"2) Data can have multiple readers and one writer."</div>
+        </PageLayout>
+    }
+}
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -2,3 +2,4 @@ pub mod intro_01;
 pub mod intro_02;
 pub mod memory_safety_03;
 pub mod intro_04;
+pub mod intro_05;
