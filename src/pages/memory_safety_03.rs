9172577408a48b7b6311616abe274d2315c3d586
Hudi304 <hudisteanum@yahoo.com>
2024-02-25 16:53:56 +0200

    Implement intro_04

--- src/pages/memory_safety_03.rs
+++ src/pages/memory_safety_03.rs
@@ -9,7 +9,7 @@ pub fn MemorySafety_03() -> impl IntoView {
             <div class="text-orange-500 text-7xl font-bold">"Memory safety"</div>
 
             <div class="grid  grid-cols-[100px_auto_auto] grid-rows-[30px_auto_auto] border">
-                <span >""</span>
+                <span>""</span>
                 <span class="border">"Memory Safe"</span>
                 <span class="border">"NOT Memory Safe"</span>
                 <span class="border">"Garbage Collected"</span>
@@ -22,18 +22,25 @@ pub fn MemorySafety_03() -> impl IntoView {
                     <img class="h-20" src="public/language_logos/python.png"/>
                     <img class="h-20 col-span-2" src="public/language_logos/Go.png"/>
                 </span>
-                <span class="border flex items-center justify-center text-5xl text-red-500">"X"</span>
+                <span class="border flex items-center justify-center text-5xl text-red-500">
+                    "X"
+                </span>
 
                 <span class="border">"NOT Garbage Collected"</span>
 
                 <span class="flex gap-3 p-3 border">
                     <img class="h-20" src="public/language_logos/rust.png"/>
-                    <img class="h-20" src="public/language_logos/zig.png"/>
+
+                    <span class="bg-white p-3 rounded-md">
+                        <img class="h-14  " src="public/language_logos/zig.png"/>
+                    </span>
+
                 </span>
 
                 <span class="flex gap-3 p-3 border">
                     <img class="w-20 h-20" src="public/language_logos/Cpp.png"/>
                     <img class="w-20 h-20" src="public/language_logos/C.png"/>
+                    "add .asm"
                 </span>
 
             </div>
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,3 +1,4 @@
 pub mod intro_01;
 pub mod intro_02;
 pub mod memory_safety_03;
+pub mod intro_04;
